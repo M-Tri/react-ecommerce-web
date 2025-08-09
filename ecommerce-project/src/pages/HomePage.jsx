5e4584649f90c62aeaac690c8fd7edbda5257be2
MT <69726672+M-Tri@users.noreply.github.com>
2025-08-09 17:29:05 -0600

    feat: get cart from the back and display quantity

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,20 +1,32 @@
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { Header } from '../components/Header'
-import { products } from '../../starting-code/data/products'
 import './HomePage.css'
 
 
 export function HomePage() {
-  axios.get('http://localhost:3000/api/products')
+
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      console.log(response.data)
+      setProducts(response.data)
     })
+
+    axios.get('http://localhost:3000/api/cart-items')
+      .then((response) => {
+        setCart(response.data)
+      })
+  }, []);
+  
 
   return (
     <>
       <title>Ecommerce Project</title>
 
-      <Header />
+      <Header cart={cart}/>
 
       <div className="home-page">
         <div className="products-grid">
