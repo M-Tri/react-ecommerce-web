73fe7bb4452cc5004e193d218b38513223de97ee
MT <69726672+M-Tri@users.noreply.github.com>
2025-08-08 20:59:51 -0600

    chore: add React StrictMode wrapper to root render

--- ecommerce-project/src/main.jsx
+++ ecommerce-project/src/main.jsx
@@ -6,7 +6,9 @@ import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
